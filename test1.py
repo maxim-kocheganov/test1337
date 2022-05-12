3ed19362c004aae9d2b81d93770e57afe5573a95
max <maxim.k.elasnce@gmail.com>
2022-05-12 17:44:52 +0300

    First test

---
+++ test1.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+import asyncio
+import aioredis
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+def anagramCheck(str1,str2):
+    if sorted(str1) == sorted(str2):
+        return True
+    else:
+        return False
+
+async def inc():
+    redis = aioredis.from_url("redis://192.168.0.102") #Place your ip
+    #await redis.set("my-key", "value")
+    #Evalue = await redis.get("my-key")
+    res = await redis.incr("countOfAn")
+    return res
+
+@app.get("/anagram/")
+async def read_item(str1: str = "", str2: str = ""):
+    anagram = anagramCheck(str1,str2)
+    if anagram:
+        count = await inc()
+        return {'anagram':'true', 'count':count}
+    else:
+        return {'anagram':'false'}
